7eb2bb1f369858d6eee500c3cde4dbc03eadba6d
noahbr62 <58325460+noahbr62@users.noreply.github.com>
2022-05-08 15:30:43 +0200

    Update logging.sh
    added missing symbols

--- bin/logging.sh
+++ bin/logging.sh
@@ -1,35 +1,35 @@
 GLEVEL=W        #Set default loglevel
 log(){
-        DATE=date '+%Y.%m.%d %H:%M:%S'
+        DATE=`date '+%Y.%m.%d %H:%M:%S'`
         LEVEL=$1
         shift
         case $LEVEL in
                 D)
                         case $LOGLEVEL in
                                 D)
-                                        echo "$DATE:DEBUG:$" >> centralLog.txt
+                                        echo "$DATE:DEBUG:$*" >> centralLog.txt
                                         ;;
                         esac
                         ;;
                 I)
                         case $LOGLEVEL in
                                 D|I)
-                                        echo "$DATE:INFO:$"  >> centralLog.txt
+                                        echo "$DATE:INFO:$*"  >> centralLog.txt
                                         ;;
                         esac
                         ;;
                 W)
                         case $LOGLEVEL in
                                 D|I|W)
-                                        echo "$DATE:WARNING:$"  >> centralLog.txt
+                                        echo "$DATE:WARNING:$*"  >> centralLog.txt
                                         ;;
                         esac
                         ;;
                 E)
-                        echo "$DATE:ERROR:$"  >> centralLog.txt
+                        echo "$DATE:ERROR:$*"  >> centralLog.txt
                         ;;
-                )
-                        echo "$DATE:UNKNOWN:$"  >> centralLog.txt
+                *)
+                        echo "$DATE:UNKNOWN:$*"  >> centralLog.txt
                         ;;
         esac
 }
