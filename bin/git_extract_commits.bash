38b626193d8271da45010fb59c50f16e20bdbd3e
Michi202 <michael.marchesi@edu.tbz.ch>
2022-05-09 11:25:49 +0200

    Added Logging File Validation and CWD

--- bin/git_extract_commits.bash
+++ bin/git_extract_commits.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
+cwd=`pwd`
 Git=($(find $3/$1 -name .git))
-Repositories=($(find repositories))
-Subdircount=$(find repositories -maxdepth 1 -type d | wc -l)
+Repositories=($(find $4))
+Subdircount=$(find $4 -maxdepth 1 -type d | wc -l)
+Log=$(find $cwd -name logging.sh)
 
 #Call Logging File
 . logging.sh
@@ -19,11 +21,23 @@ else
   log W repository does not exist
 fi
 
+#Check if Logging Script exists
+if [ -e "$Log" ]; then
+  LOGLEVEL=I
+  :
+  log I log script exists
+else
+  LOGLEVEL=W
+  echo "No logging script, cannot log"
+  log W repository does not exist
+fi
+
+
 #Check if GIT Repositories are in Repository
 if [[ "$Subdircount" -eq 1 ]]
 then
     LOGLEVEL=W
-    echo "No Git Repositories"
+    echo "No git repositories"
     log W no git repositories in repository
 else
     LOGLEVEL=I
@@ -31,16 +45,13 @@ else
     log I git repositories in repository
 fi
 
-#Define Header for Output File
-echo "Zielverzeichnis,Datum,Commit-Hash,Author" >> $2
-
 #For each repository call git log
 for repository in $Git ;
 do
+    LOGLEVEL=S
+    log S successfully extracted commits
     path=$(sed 's/.git//g' <<< $repository)
     cd $path
     base=$(basename $path);
-    git log --pretty=format:$base',%cd,%H,%an' --date=format:'%Y%m%d' >> $2
-    LOGLEVEL=S
-    log S successfully extracted commits
+    git log --pretty=format:$base',%cd,%H,%an' --date=format:'%Y%m%d' > $2
 done
